880cdd1b6cabcc3d901613d41283b0da250e4897
THE-HYPER-NEW <Animechillax728@gmail.com>
2025-03-13 20:20:15 +0530

    Update script.py

--- utils/script.py
+++ utils/script.py
@@ -1,15 +1,19 @@
 class script(object):
-    START = """**Hello {} 🤟**
+    START = """**Hello {} 🥀**
    
 I am **Find Post Bot**.I am best Channel Link Search Bot! 
-I Will filter your channel posts automatically and send it in your group chat when Someone search it."""
+I Will filter your channel posts automatically and send it in your group chat when Someone search it I Am undermaintenance in ACX Network."""
 
     HELP = """To Use me In A Group
 
 - Add me in your group & channel with all permissions. 
-- Send /verify in group & wait for It To Accept Or Directly Contact To Owner After Request @GreyMatter_Bots.
+
+- Send /verify in group & wait for It To Accept Or Directly Contact To Owner After Request @THEHYPER_ACX 
+
 - After verification send /connect YourChannelID
+
 - Example : /connect -100xxxxxxxxxx
+
 - Done ✅. <b><I>Enjoy 💜❤</I></b> )
 
 Remove a Channel with - /disconnect-100xxxxxxxxxxx
@@ -17,11 +21,11 @@ this will help you to remove a indexed channel from your group.
 
 Get connected channels list with - /connections"""
 
-    ABOUT = """Developed By @CyniteBackup
+    ABOUT = """Developed By @THEHYPER_ACX 
 
 ✯ Mʏ Nᴀᴍᴇ:  {}
-✯ Cʀᴇᴀᴛᴏʀ: <a href='https://t.me/CyniteOfficial'>Harman</a>
-✯ Lᴀɴɢᴜᴀɢᴇ: <a href='https://www.python.org'>Pʏᴛʜᴏɴ 3</a>
+✯ Cʀᴇᴀᴛᴏʀ: <a href='https://t.me/THEHYPER_ACX>𝗧ʜᴇＨʏᴘᴇʀ</a>
+✯ Cʜᴀɴɴᴇʟ: <a href='https://t.me/ACX_NETWORK'>ACX Nᴇᴛᴡᴏʀᴋ</a>
 ✯ DᴀᴛᴀBᴀsᴇ: <a href='https://cloud.mongodb.com/'>MᴏɴɢᴏDB</a>
 ✯ Bᴏᴛ Sᴇʀᴠᴇʀ: : <a href='https://heroku.com'>Heroku</a>"""
 
